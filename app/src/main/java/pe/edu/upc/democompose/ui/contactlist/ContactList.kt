2c355e2916d73b5cd818f911cf4a2f799367362e
Lucero Obispo <91815464+luceroobispo@users.noreply.github.com>
2024-04-08 17:45:52 -0500

    feat: Add Swipe Delete

--- app/src/main/java/pe/edu/upc/democompose/ui/contactlist/ContactList.kt
+++ app/src/main/java/pe/edu/upc/democompose/ui/contactlist/ContactList.kt
@@ -1,6 +1,12 @@
 package pe.edu.upc.democompose.ui.contactlist
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -9,6 +15,8 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
+import androidx.compose.material3.DismissDirection
+import androidx.compose.material3.DismissState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -17,9 +25,23 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import pe.edu.upc.democompose.model.Contact
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.DismissValue
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SwipeToDismiss
+import androidx.compose.material3.rememberDismissState
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import kotlinx.coroutines.delay
 
 @Composable
 fun ContactList(
@@ -27,8 +49,6 @@ fun ContactList(
     newContact: () -> Unit,
     selectContact: (Int) -> Unit
 ) {
-
-
     Scaffold(floatingActionButton = {
         FloatingActionButton(onClick = { newContact() }) {
             Icon(Icons.Filled.Add, "New contact")
@@ -36,15 +56,18 @@ fun ContactList(
     }) { paddingValues ->
         LazyColumn(modifier = Modifier.padding(paddingValues)) {
             itemsIndexed(contacts.value) { index, contact ->
-                ContactItem(contact, selectContact = {
-                    selectContact(index)
-                })
+                SwipeToDeleteContainer(
+                    item = contact,
+                    onDelete = {
+                        contacts.value = contacts.value.filterIndexed { idx, _ -> idx != index }.toTypedArray()
+                    }
+                ) {
+                    ContactItem(contact = it, selectContact = { selectContact(index) })
+                }
             }
         }
-
     }
 }
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ContactItem(contact: Contact, selectContact: () -> Unit) {
@@ -55,5 +78,77 @@ fun ContactItem(contact: Contact, selectContact: () -> Unit) {
             Text(text = contact.name, modifier = Modifier.fillMaxWidth())
             Text(text = contact.telephone, modifier = Modifier.fillMaxWidth())
         }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun <T> SwipeToDeleteContainer(
+    item: T,
+    onDelete: (T) -> Unit,
+    animationDuration: Int = 500,
+    content: @Composable (T) -> Unit
+) {
+    var isRemoved by remember {
+        mutableStateOf(false)
+    }
+    val state = rememberDismissState(
+        confirmValueChange = { value ->
+            if (value == DismissValue.DismissedToStart) {
+                isRemoved = true
+                true
+            } else {
+                false
+            }
+        }
+    )
+
+    LaunchedEffect(key1 = isRemoved) {
+        if(isRemoved) {
+            delay(animationDuration.toLong())
+            onDelete(item)
+        }
+    }
+
+    AnimatedVisibility(
+        visible = !isRemoved,
+        exit = shrinkVertically(
+            animationSpec = tween(durationMillis = animationDuration),
+            shrinkTowards = Alignment.Top
+        ) + fadeOut()
+    ) {
+        SwipeToDismiss(
+            state = state,
+            background = {
+                DeleteBackground(swipeDismissState = state)
+            },
+            dismissContent = { content(item) },
+            directions = setOf(DismissDirection.EndToStart)
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DeleteBackground(
+    swipeDismissState: DismissState
+) {
+    val color = if (swipeDismissState.dismissDirection == DismissDirection.EndToStart) {
+        Color.Red
+    } else Color.Transparent
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color)
+            .padding(16.dp),
+        contentAlignment = Alignment.CenterEnd
+    ) {
+        Icon(
+            imageVector = Icons.Default.Delete,
+            contentDescription = null,
+            tint = Color.White
+        )
     }
 }
